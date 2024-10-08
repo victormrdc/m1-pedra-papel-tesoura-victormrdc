9bc07eeeb0b710998c67fd255fbf4623dff29462
Victor Costa <vitomrdc@gmail.com>
2024-10-08 16:38:25 -0300

    funcao rockpaperscissor rodando

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-/* Desenvolva seu código abaixo */
-console.log("Primeira entrega!")
+
+function playRockPaperScissor(player1, player2) {
+
+    if (
+        (player1 == "Pedra" && player2 == "Tesoura") ||
+        (player1 == "Tesoura" && player2 == "Papel") ||
+        (player1 == "Papel" && player2 == "Pedra")
+    ) {
+        return "Jogador 1 venceu!"
+
+    } else if (
+        (player1 == "Tesoura" && player2 == "Pedra") ||
+        (player1 == "Papel" && player2 == "Tesoura") ||
+        (player1 == "Pedra" && player2 == "Papel")
+    ) {
+        return "Jogador 2 venceu!"
+
+    } else {
+        return "Empate!"
+    }
+}
+
+const resultado = playRockPaperScissor("Pedra", "Pedra")
+console.log(resultado);
